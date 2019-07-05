edbf3c132972ae3e55851fe01d2e884cd1bb1e24
pantianyi <pantianyi@sodao.com>
2019-07-05 14:00:50 +0800

    test:添加单元测试

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,16 +1,24 @@
-'use strict';
-const path = require('path');
-const assert = require('yeoman-assert');
-const helpers = require('yeoman-test');
+"use strict";
+const path = require("path");
+const assert = require("yeoman-assert");
+const helpers = require("yeoman-test");
 
-describe('generator-vue-webpack-cli:app', () => {
+describe("generator-vue-webpack-cli:app", () => {
   beforeAll(() => {
     return helpers
-      .run(path.join(__dirname, '../generators/app'))
+      .run(path.join(__dirname, "../generators/index.js"))
       .withPrompts({ someAnswer: true });
   });
 
-  it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+  it("creates files", () => {
+    assert.file(["src"]);
+    assert.file(["config"]);
+    assert.file([".babelrc"]);
+    assert.file([".gitignore"]);
+    assert.file([".eslintrc.js"]);
+    assert.file(["postcss.config.js"]);
+    assert.file(["index.html"]);
+    assert.file(["README.md"]);
+    assert.file(["package.json"]);
   });
 });
